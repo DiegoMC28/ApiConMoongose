19647ad811f8c5913d2f118c7edd115b54c8e1b5
DiegoMC28 <diego010128@gmail.com>
2023-02-10 17:43:27 +0100

    *

--- DiegoMata-APIconMongoose/src/models/user.js
+++ DiegoMata-APIconMongoose/src/models/user.js
@@ -1,47 +1,56 @@
 const mongoose = require('mongoose')
-const mongodb = require('mongodb')
 const validator = require('validator')
-const Course = require('./course')
+const bcrypt = require('bcryptjs')
 
-const User = mongoose.model('User', {
-    dni: {
-        type: String,
-        required: true,
-        minlength: 9,
-        trim: true,
-        validate(value = '') {
-            if (validator.isIdentityCard(value, 'ES')) {
-                throw new Error('El dni no es valido')
-            }
-        }
-    },
+const userSchema = new mongoose.Schema({
     nombre: {
         type: String,
         required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
         trim: true,
-        minlength: 3
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('El Email es incorrecto')
+            }
+        }
+    },
+    contraseña: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('contraseña')) {
+                throw new Error('Contraseña no contiene "contraseña"')
+            }
+        }
     },
     edad: {
         type: Number,
         default: 0,
-        required: true,
-        validate(value = 0) {
+        validate(value) {
             if (value < 0) {
-                throw new Error('La edad debe ser un numero mayor que 0')
-            }
-        }
-    }, 
-    telefono: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 9,
-        validate(value = '') {
-            if (!validator.isMobilePhone(value)) {
-                throw new Error('El telefono es incorrecto')
+                throw new Error('La edad debe ser un numero positivio')
             }
         }
     }
 })
+
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('contraseña')) {
+        user.contraseña = await bcrypt.hash(user.contraseña, 8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
